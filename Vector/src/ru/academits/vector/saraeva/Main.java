573b7265789177d49668db5c26252a85c4669594
ESaraeva <zakamen22@gmail.com>
2017-03-15 16:38:50 +0700

    Функция сложения (пока без возможности складывать массивы разной длины) векторов, умножения вектора на скаляр, разаорота вектора, toString

--- Vector/src/ru/academits/vector/saraeva/Main.java
+++ Vector/src/ru/academits/vector/saraeva/Main.java
@@ -7,16 +7,21 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        double[] arr = {1, 9};
+        double[] arr = {1, 9, 3, 2, 2, 7};
         double[] arr2 = {1, 4, 0, 11, 6, 8};
         Vector vector3 = new Vector(arr);
         Vector vector = new Vector(arr2);
-        for (int i = 0; i < vector.getSize(); ++i) {
-            System.out.println(vector.toString());
-        }
-        System.out.println(vector3.getSize());
-        vector.multiplyByScalar(4);
-        //System.out.print(Arrays.toString(vector.totUpVectors(arr)));
+        /*System.out.println(vector3.toString());
+        System.out.print(vector.toString());
+        System.out.println();
+        vector3.multiplyByScalar(3);
+        System.out.println(vector3);
+        vector3.rotateVector();
+        System.out.println(vector3);
+        System.out.println();
+        System.out.println(vector3.getSize());*/
+        vector.totUpVectors(vector3);
+        System.out.println(vector);
 
     }
 }
--- Vector/src/ru/academits/vector/saraeva/Vector.java
+++ Vector/src/ru/academits/vector/saraeva/Vector.java
@@ -6,7 +6,6 @@ package ru.academits.vector.saraeva;
 public class Vector {
     private double[] vector;
 
-
     public Vector(int dimension) {                // конструктор а
         this.vector = new double[dimension];
     }
@@ -15,39 +14,52 @@ public class Vector {
         this(vectorObject.vector);
     }
 
-
     public Vector(double[] vectorArray) {
         this.vector = new double[vectorArray.length];
         System.arraycopy(vectorArray, 0, this.vector, 0, vectorArray.length);
     }
 
-
     public Vector(int dimension, double[] vectorArray) {     //конструктор d
         this.vector = new double[dimension];
+
         System.arraycopy(vectorArray, 0, this.vector, 0, Math.min(vector.length, vectorArray.length));
     }
 
     public String toString() {
-
-        return "{" + vector + "}";
-    }
-
-    public void totUpVectors(double... vector2) {
-
-        int resultDimension = Math.max(vector.length, vector2.length);
-        double[] vectorResult = new double[resultDimension];
-        for (int i = 0; i < Math.max(vector.length, vector2.length); ++i) {
-            vectorResult[i] = vector[i] + vector2[i];
+        StringBuilder builder = new StringBuilder();
+        builder.append('{');
+        for (double aVector : vector) {
+            builder.append(aVector);
+            builder.append(',');
+            builder.append(' ');
         }
+        builder.delete(builder.length() - 2, builder.length());
+        builder.append('}');
+        return builder + "";
     }
 
-    public int getSize() {
-        return vector.length;
-    }
+
+            public void totUpVectors(Vector vector2) {
+         int resultDimension = Math.max(vector.length, vector2.getSize());
+         double[] vectorResult = new double[resultDimension];
+         for (int i = 0; i < Math.max(vector.length, vector2.getSize()); ++i) {
+             vector[i] = vector[i] + vector2.vector[i];
+         }
+     }
+
 
     public void multiplyByScalar(double scalar) {
         for (int i = 0; i < vector.length; ++i) {
             vector[i] = scalar * vector[i];
         }
+    }
+    public void rotateVector(){
+        for (int i = 0; i < vector.length; ++i) {
+            vector[i] = -1 * vector[i];
+        }
+    }
+
+    public int getSize() {
+        return vector.length;
     }
 }
